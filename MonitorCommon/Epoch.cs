0b5e1597505746bc0906288a75b8a1a56aee4f3b
Slava Kolobaev <gmanyy@gmail.com>
2021-10-04 08:57:14 +0200

    Added microsecond epoch time convert methods.

--- MonitorCommon/Epoch.cs
+++ MonitorCommon/Epoch.cs
@@ -10,10 +10,12 @@ namespace MonitorCommon
 
         public static long ToEpoch(this DateTime time) => (long) (time - Start).TotalSeconds;
         public static long ToEpochMs(this DateTime time) => (long) (time - Start).TotalMilliseconds;
+        public static long ToEpochMcs(this DateTime time) => ToEpochFileTicks(time) / 10;
         public static long ToEpochFileTicks(this DateTime time) => time.Ticks - StartTicks;
 
         public static DateTime AsEpoch(this long epoch) => Start.AddSeconds(epoch);
         public static DateTime AsEpochMs(this long epoch) => Start.AddMilliseconds(epoch);
+        public static DateTime AsEpochMcs(this long epoch) => AsEpochFileTicks(epoch * 10);
         public static DateTime AsEpochFileTicks(this long epoch) => new DateTime(epoch + StartTicks, DateTimeKind.Utc);
 
         public static long NowFileTicks
